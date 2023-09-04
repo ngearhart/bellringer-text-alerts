6128a60c5dbe21a066a77395adaf32b02f50c74b
Noah Gearhart <noah.gearhart@darkwolfsolutions.com>
2023-09-04 19:50:50 -0400

    Change authDomain to fix safari problem

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,7 +2,9 @@ import { initializeApp } from 'firebase/app'
 
 export const firebaseApp = initializeApp({
     apiKey: "AIzaSyBC5R5IteQpeITUNaQlUoWihq0mpa5hTRY",
-    authDomain: "bellringer-text-alerts.firebaseapp.com",
+    // https://firebase.google.com/docs/auth/web/redirect-best-practices#web-modular-api
+    // Fix Safari issue
+    authDomain: "text-alerts.bellringer.noahgearhart.com",  // TODO: figure out how to change this locally
     databaseURL: "https://bellringer-text-alerts-default-rtdb.firebaseio.com",
     projectId: "bellringer-text-alerts",
     storageBucket: "bellringer-text-alerts.appspot.com",
